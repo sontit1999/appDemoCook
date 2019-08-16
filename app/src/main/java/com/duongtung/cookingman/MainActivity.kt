452808471435bb2cc0d98f30431183cacd7d0941
Sontitxxxx <sonkoii1995@gmail.com>
2019-08-16 18:21:29 +0700

    commit custom

--- app/src/main/java/com/duongtung/cookingman/MainActivity.kt
+++ app/src/main/java/com/duongtung/cookingman/MainActivity.kt
@@ -2,9 +2,11 @@ package com.duongtung.cookingman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
