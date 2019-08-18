ef730aa6b860ca4ebd5bef6abc2798c82fde1d6c
chaienoto <50574624+chaienoto@users.noreply.github.com>
2019-08-19 00:37:45 +0700

    splash

---
+++ app/src/main/java/com/duongtung/cookingman/ui/splash/SplashViewModel.kt
@@ -0,0 +1,14 @@
+package com.duongtung.cookingman.ui.splash
+
+import android.util.Log
+import com.duongtung.cookingman.base.ui.base.BaseViewModel
+
+class SplashViewModel : BaseViewModel() {
+    fun handleGetStarted() {
+        Log.d("","handleGetStarted")
+    }
+    fun handleSignIn()
+    {
+        Log.d("","handleSignIn")
+    }
+}
