2d5c971d05100226da7906793e0b6ac0cb4ece2b
Sontitxxxx <sonkoii1995@gmail.com>
2019-08-29 14:36:46 +0700

    fix loi bindding

--- app/src/main/java/com/duongtung/cookingman/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/duongtung/cookingman/ui/splash/SplashActivity.kt
@@ -1,12 +1,9 @@
 package com.duongtung.cookingman.ui.splash
 
 import android.content.Intent
-import android.util.Log
 import com.duongtung.cookingman.R
 import com.duongtung.cookingman.base.BaseActivity
 import com.duongtung.cookingman.databinding.ActivitySplashBinding
-import com.duongtung.cookingman.model.Ingredient
-import com.duongtung.cookingman.ui.chatdetail.DetailChatActivity
 import com.duongtung.cookingman.ui.login.LoginActivity
 import com.duongtung.cookingman.ui.recipefood.RecipeActivity
 
