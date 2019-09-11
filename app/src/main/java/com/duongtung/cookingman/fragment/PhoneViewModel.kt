bd60a3e783b3b6d24952a1d9b7c9e627b224c658
Tung Duong <tung.duong@tripath.vn>
2019-09-11 17:47:02 +0700

    fix font

--- app/src/main/java/com/duongtung/cookingman/fragment/PhoneViewModel.kt
+++ app/src/main/java/com/duongtung/cookingman/fragment/PhoneViewModel.kt
@@ -1,10 +1,16 @@
 package com.duongtung.cookingman.fragment
 
 import android.R
+import android.view.View
 import android.widget.ArrayAdapter
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.duongtung.cookingman.base.BaseApplication
 import com.duongtung.cookingman.base.ui.base.BaseViewModel
 
 class PhoneViewModel : BaseViewModel(){
-
+    fun handleNext(){
+//        NavHostFragment.findNavController(VerifyFragment())
+    }
 }
--- app/src/main/java/com/duongtung/cookingman/ui/login/LoginActivity.kt
+++ app/src/main/java/com/duongtung/cookingman/ui/login/LoginActivity.kt
@@ -1,20 +1,16 @@
 package com.duongtung.cookingman.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import androidx.viewpager.widget.ViewPager
 import com.duongtung.cookingman.R
 import com.duongtung.cookingman.base.BaseActivity
 import com.duongtung.cookingman.databinding.ActivityLoginBinding
 import com.duongtung.cookingman.fragment.PhoneFragment
 import com.duongtung.cookingman.fragment.PreferanceFragment
 import com.duongtung.cookingman.fragment.VerifyFragment
-//import devmike.jade.com.PageStepIndicator
 
-class LoginActivity : BaseActivity<ActivityLoginBinding,LoginViewModel>() {
+class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>() {
     override fun getViewMode() = LoginViewModel::class.java
 
     override fun getLayout() = R.layout.activity_login
@@ -25,11 +21,12 @@ class LoginActivity : BaseActivity<ActivityLoginBinding,LoginViewModel>() {
         binding.pageStepper.setupWithViewPager(binding.viewPage)
     }
 
-    class ViewPageAdapter(fm : FragmentManager) : FragmentStatePagerAdapter(fm) {
+    class ViewPageAdapter(fm: FragmentManager) :
+        FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
         val pageLists = arrayListOf(PhoneFragment(), VerifyFragment(), PreferanceFragment())
 
         override fun getItem(position: Int): Fragment {
-           return pageLists.get(position)
+            return pageLists.get(position)
         }
 
         override fun getCount(): Int {
