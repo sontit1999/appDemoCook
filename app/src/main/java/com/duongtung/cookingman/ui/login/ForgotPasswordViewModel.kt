488be46cfb6bb17d66d159cb77ae1c70d92c729f
Sontitxxxx <sonkoii1995@gmail.com>
2019-09-15 17:34:41 +0700

    forgotpassactivity and SendEmailAcrivity

---
+++ app/src/main/java/com/duongtung/cookingman/ui/login/ForgotPasswordViewModel.kt
@@ -0,0 +1,5 @@
+package com.duongtung.cookingman.ui.login
+
+import com.duongtung.cookingman.base.ui.base.BaseViewModel
+
+class ForgotPasswordViewModel  : BaseViewModel(){}
--- app/src/main/java/com/duongtung/cookingman/ui/login/LoginEmailActivity.kt
+++ app/src/main/java/com/duongtung/cookingman/ui/login/LoginEmailActivity.kt
@@ -2,7 +2,11 @@ package com.duongtung.cookingman.ui.login
 
 import com.duongtung.cookingman.R
 import com.duongtung.cookingman.base.BaseActivity
+import com.duongtung.cookingman.databinding.ActivityForgotPasswordBinding
+import com.duongtung.cookingman.databinding.ActivityLoginBindingImpl
 import com.duongtung.cookingman.databinding.ActivityLoginnBinding
+import com.duongtung.cookingman.databinding.ActivityLoginnBindingImpl
+import kotlinx.android.synthetic.main.activity_loginn.view.*
 
 class LoginEmailActivity : BaseActivity<ActivityLoginnBinding,LoginEmailViewModel>(){
     override fun getViewMode() = LoginEmailViewModel::class.java
@@ -10,7 +14,10 @@ class LoginEmailActivity : BaseActivity<ActivityLoginnBinding,LoginEmailViewMode
     override fun getLayout() = R.layout.activity_loginn
 
     override fun setBindingViewModel() {
-       binding.viewModel = viewModel
+        binding.viewModel = viewModel
+        binding.tvForgetpass.setOnClickListener {
+            goToActivity(ForgotPasswordActivity::class.java,null,null)
+        }
     }
 
 }
